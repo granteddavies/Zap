dcc713312e8aebddcf8355fea440c2312454b315
granteddavies <granteddavies@gmail.com>
2016-04-18 21:54:32 -0500

    Added a static class to hold the user's User object called Profile. Added logic in login to load the user's data and initialize the Profile

--- app/src/main/java/com/zap/LoginActivity.java
+++ app/src/main/java/com/zap/LoginActivity.java
@@ -9,8 +9,15 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -26,7 +33,7 @@ public class LoginActivity extends AppCompatActivity {
 
         // Check if the user is already logged in
         if (AccessToken.getCurrentAccessToken() != null) {
-            startMainActivity();
+            loadProfile();
         }
 
         callbackManager = CallbackManager.Factory.create();
@@ -35,7 +42,7 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                startMainActivity();
+                loadProfile();
             }
 
             @Override
@@ -61,5 +68,25 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void loadProfile() {
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        try {
+                            JSONObject jObj = response.getJSONObject();
+                            Profile.user = new User(jObj.getString("name"), jObj.getString("id"));
+                            startMainActivity();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).executeAsync();
     }
 }
---
+++ app/src/main/java/com/zap/Profile.java
@@ -0,0 +1,8 @@
+package com.zap;
+
+/**
+ * Created by Grant on 4/18/2016.
+ */
+public final class Profile {
+    public static User user;
+}
